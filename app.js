e476a76b5c9227e176b7c2115c3c531cd15eda8a
sbuckley <sbuckley65@hotmail.com>
2016-04-15 13:45:43 -0700

    Added sum function.

---
+++ app.js
@@ -0,0 +1,9 @@
+var firstnum;
+var secondnum;
+var thirdnum;
+
+function sum(firstnum,secondnum) {
+  thirdnum = firstnum + secondnum;
+  console.log('The sum of ' + firstnum + ' and ' + secondnum + ' is ' + thirdnum + '.');
+  return thirdnum;
+}
