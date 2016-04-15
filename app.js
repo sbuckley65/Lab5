657ed8d61de6e5d6bf7e6829f4c44ce615fc5c6f
sbuckley <sbuckley65@hotmail.com>
2016-04-15 15:44:18 -0700

    added fourth function.

--- app.js
+++ app.js
@@ -21,3 +21,13 @@ function sumAndMultiply(firstnum,secondnum,thirdnum) {
   console.log(firstnum + ' and ' + secondnum + ' and ' + thirdnum + ' sum to ' + sam[0] + '.');
   console.log('The numbers ' + firstnum + ' and ' + secondnum + ' and ' + thirdnum + ' have a product of ' + fifthnum + '.');
 }
+
+function sumArray(table) {
+  var sixthnum = 0;
+  for (var i = 0; i < table.length; i++) {
+    sixthnum = sixthnum + table[i];
+  }
+  console.log(table + ' was passed in as an array of numbers, and ' + sixthnum + ' is their sum.');
+  console.table([table]);
+  return sixthnum;
+}
