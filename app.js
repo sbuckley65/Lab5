1bb0848a4b11e0b5cc25ba7c065a44e3157e3090
sbuckley <sbuckley65@hotmail.com>
2016-04-15 20:02:43 -0700

    Added final question and replace text feature.

--- app.js
+++ app.js
@@ -4,22 +4,21 @@ var thirdnum;
 
 function sum(firstnum,secondnum) {
   thirdnum = firstnum + secondnum;
-  // console.log('The sum of ' + firstnum + ' and ' + secondnum + ' is ' + thirdnum + '.');
+  document.getElementById('par1').textContent = ('The sum of ' + firstnum + ' and ' + secondnum + ' is ' + thirdnum + '.');
   return thirdnum;
 }
 
 function multiply(firstnum,secondnum) {
-  forthnum = firstnum * secondnum;
-  // console.log('The product of ' + firstnum + ' and ' + secondnum + ' is ' + thirdnum + '.');
-  return forthnum;
+  thirdnum = firstnum * secondnum;
+  document.getElementById('par2').textContent = ('The product of ' + firstnum + ' and ' + secondnum + ' is ' + thirdnum + '.');
+  return thirdnum;
 }
 
 function sumAndMultiply(firstnum,secondnum,thirdnum) {
   var forthnum = sum(sum(firstnum,secondnum),thirdnum);
   var fifthnum = multiply(multiply(firstnum,secondnum),thirdnum);
   var sam = [forthnum,fifthnum];
-  console.log(firstnum + ' and ' + secondnum + ' and ' + thirdnum + ' sum to ' + sam[0] + '.');
-  console.log('The numbers ' + firstnum + ' and ' + secondnum + ' and ' + thirdnum + ' have a product of ' + fifthnum + '.');
+  document.getElementById('par3').textContent = (firstnum + ' and ' + secondnum + ' and ' + thirdnum + ' sum to ' + sam[0] + '. The numbers ' + firstnum + ' and ' + secondnum + ' and ' + thirdnum + ' have a product of ' + fifthnum + '.');
 }
 
 function sumArray(table) {
@@ -27,7 +26,17 @@ function sumArray(table) {
   for (var i = 0; i < table.length; i++) {
     sixthnum = sixthnum + table[i];
   }
-  console.log(table + ' was passed in as an array of numbers, and ' + sixthnum + ' is their sum.');
+  document.getElementById('par4').textContent = (table + ' was passed in as an array of numbers, and ' + sixthnum + ' is their sum.');
+  console.table([table]);
+  return sixthnum;
+}
+
+function multiplyArray(table) {
+  var sixthnum = 0;
+  for (var i = 0; i < table.length; i++) {
+    sixthnum = sixthnum * table[i];
+  }
+  document.getElementById('par5').textContent = (table + ' was passed in as an array of numbers, and ' + sixthnum + ' is their product.');
   console.table([table]);
   return sixthnum;
 }
